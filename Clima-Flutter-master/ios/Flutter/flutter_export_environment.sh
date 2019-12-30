d3fe51f849f4f27b8a2380e95f051fbde22d13b1
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-30 16:33:23 +0900

    add Clima project

---
+++ Clima-Flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/yamamototatsuya/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yamamototatsuya/workspace/08_Flutter/flutter_practice_app/Clima-Flutter-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yamamototatsuya/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
