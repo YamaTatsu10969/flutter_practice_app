844dacf2f2f54a80a3115c95275ef602d4caf088
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-31 14:32:12 +0900

    complete bitcoin

--- bitcoin-ticker-flutter/lib/coin_data.dart
+++ bitcoin-ticker-flutter/lib/coin_data.dart
@@ -1,3 +1,5 @@
+import 'networking.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +30,26 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const baseURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+
+enum Symbol {
+  BTC,
+  ETH,
+  LTC,
+}
+
+class CoinData {
+  CoinData({this.country, this.crypto});
+  final String country;
+  final String crypto;
+  double price;
+
+  Future getPrice() async {
+    String query = crypto + country;
+    NetworkHelper networking = NetworkHelper(baseURL + query);
+    var data = await networking.getData();
+    print(data);
+    price = await data['last'];
+    return price;
+  }
+}
--- bitcoin-ticker-flutter/lib/price_screen.dart
+++ bitcoin-ticker-flutter/lib/price_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +11,86 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedItem = 'USD';
+  String btcPrice;
+  String ethPrice;
+  String ltcPrice;
+
+  Future updateUI() async {
+    // setStateの前に通信処理を行う
+    var btc = await CoinData(country: selectedItem, crypto: 'BTC').getPrice();
+    var eth = await CoinData(country: selectedItem, crypto: 'ETH').getPrice();
+    var ltc = await CoinData(country: selectedItem, crypto: 'LTC').getPrice();
+    try {
+      // setStateでは、state管理している処理だけ行う！！！
+      setState(() {
+        btcPrice = btc.toString();
+        ethPrice = eth.toString();
+        ltcPrice = ltc.toString();
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI();
+    print('呼ばれてないよ〜😈');
+  }
+
+  List<CoinCard> getCoinCards() {
+    List<CoinCard> coinCards = [];
+    for (String cryp in cryptoList) {
+      coinCards.add(
+        CoinCard(
+          coinData: CoinData(country: selectedItem, crypto: cryp),
+        ),
+      );
+    }
+    return coinCards;
+  }
+
+  DropdownButton androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      dropdownItems.add(DropdownMenuItem(
+          child: Text(
+            currency,
+            style: TextStyle(color: Colors.white),
+          ),
+          value: currency));
+    }
+
+    return DropdownButton(
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedItem = value;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) async {
+        print(selectedIndex);
+        selectedItem = currenciesList[selectedIndex];
+        updateUI();
+      },
+      children: pickerItems,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,36 +101,119 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                child: Card(
+                  color: Colors.lightBlueAccent,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 BTC = $btcPrice  $selectedItem',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                child: Card(
+                  color: Colors.lightBlueAccent,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 ETH = $ethPrice $selectedItem',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                child: Card(
+                  color: Colors.lightBlueAccent,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      "1 LTC = $ltcPrice  $selectedItem",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iOSPicker() : androidDropdown()),
         ],
       ),
     );
   }
 }
+
+class CoinCard extends StatelessWidget {
+  CoinCard({@required this.coinData});
+  CoinData coinData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 ${coinData.crypto} = ${coinData.price} ${coinData.country}',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// 1. 通信を作る
+// 2var 反映させる
+// 必要なもの、bitcoin model は、
