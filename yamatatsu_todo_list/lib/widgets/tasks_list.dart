f1d899ae9992b691fa99bc202f15d2d0188a5479
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-01-04 22:03:13 +0900

    add

--- yamatatsu_todo_list/lib/widgets/tasks_list.dart
+++ yamatatsu_todo_list/lib/widgets/tasks_list.dart
@@ -7,7 +7,7 @@ class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
-      builder: (context, taskData, child) {
+      builder: (context, taskData, _) {
         return ListView.builder(
           itemBuilder: (context, index) {
             final task = taskData.tasks[index];
