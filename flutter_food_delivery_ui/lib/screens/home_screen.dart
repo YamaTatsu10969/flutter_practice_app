fac49a5b52b4764a4dfdc28edc6d9b455e285a91
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-03-16 23:42:15 +0900

    fi

--- flutter_food_delivery_ui/lib/screens/home_screen.dart
+++ flutter_food_delivery_ui/lib/screens/home_screen.dart
@@ -1,182 +1,53 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_food_delivery_ui/screens/cart_screen.dart';
-import 'package:flutter_food_delivery_ui/screens/restaurant_screen.dart';
-import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
-import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
-
-import '../data/data.dart';
-import '../models/restaurant.dart';
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  _buildRestaurants() {
-    List<Widget> restaurantList = [];
-    restaurants.forEach((Restaurant restaurant) {
-      restaurantList.add(
-        GestureDetector(
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => RestaurantScreen(restaurant: restaurant),
-            ),
-          ),
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(
-                width: 1,
-                color: Colors.grey[200],
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Hero(
-                    tag: restaurant.imageUrl,
-                    child: Image(
-                      height: 150,
-                      width: 150,
-                      image: AssetImage(restaurant.imageUrl),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Flexible(
-                  child: Container(
-                    margin: EdgeInsets.all(12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          restaurant.name,
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        RatingStars(restaurant.rating),
-                        Text(
-                          restaurant.address,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          '0.2 miles away',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      );
-    });
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(children: restaurantList),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.account_circle),
-          iconSize: 30.0,
-          onPressed: () {},
-        ),
-        title: Text('Food Delivery'),
-        actions: <Widget>[
-          FlatButton(
-              onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => CartScreen(),
-                  )),
-              child: Text(
-                'Cart (${currentUser.cart.length})',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ))
-        ],
-      ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 15),
-                fillColor: Colors.white,
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(width: 0.8),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(
-                    width: 0.8,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-                hintText: 'Search Food or Restaurants',
-                prefixIcon: Icon(
-                  Icons.search,
-                  size: 30.0,
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.clear),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-          ),
-          RecentOrders(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  'Nearby Restaurants',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.2,
-                  ),
-                ),
-              ),
-              _buildRestaurants(),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+#管理者
+create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  expose :name",              null: false, default: ""
+  expose :email",              null: false, default: ""
+  expose :encrypted_password", null: false, default: ""
+  t.datetime "remember_created_at"
+end
+#一般ユーザー
+create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"  do |t|
+  # Device関連
+  t.string "provider",           null: false, default: "email"
+  t.string "uid",                null: false, default: ""
+  t.text :tokens
+  t.string "email",              null: false, default: ""
+  t.string "encrypted_password", null: false, default: ""
+  t.string "reset_password_token"
+  t.datetime "reset_password_sent_at"
+  t.datetime "remember_created_at"
+  t.integer "sign_in_count", default: 0, null: false
+  t.datetime "current_sign_in_at"
+  t.datetime "last_sign_in_at"
+  t.string "current_sign_in_ip"
+  t.string "last_sign_in_ip"
+  t.string "confirmation_token"
+  t.datetime "confirmed_at"
+  t.datetime "confirmation_sent_at"
+  t.string "unconfirmed_email"
+  t.integer "failed_attempts", default: 0, null: false
+  t.string "unlock_token"
+  t.datetime "locked_at"
+  t.timestamps null: false
+  # Push通知関連
+  t.string :endpoint_arn
+  t.string :device_token
+  t.string :mobile_os
+  t.string "phone_number"
+  t.string "nickname"
+  t.integer "gender"
+  t.date "date_of_birth"
+  t.integer "come_from"
+  t.integer "residence"
+  t.string "full_name"
+  t.string "alphabet_full_name"
+  t.integer "school_id"
+  t.integer "temporary_school_id"
+  t.datetime "online_at"
+  t.datetime "offline_at"
+  t.boolean "introduce_user", null: false, default: false
+end
+add_index :users, :email,                unique: true
+add_index :users, :reset_password_token, unique: true
+add_index :users, :confirmation_token,   unique: true
+add_index :users, :unlock_token,         unique: true
